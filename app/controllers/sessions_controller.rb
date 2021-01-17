8d04a13d0d979ae7caa8b9246d4a127d8884fdb0
kensuke suzuki <35k.0101@gmail.com>
2021-01-17 13:32:41 +0900

    ログイン機能修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :authenticate_user, {only: [:edit, :destroy]}
+
   def new
     if logged_in?
       redirect_to blogs_path
@@ -7,8 +9,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    session[:user_id] = user.id
-    redirect_to blogs_path
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to blogs_path
+    else
+      flash[:danger] = "ログインに失敗しました。"
+      render :new
+    end
   end
   
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "blogs#index"
+  root "users#new"
   resources :blogs
   resources :users
   resources :sessions
